7c16216c115eb1f33f66fbca40768c8c0921e8e2
lijohnttle <lijohnttle@outlook.com>
2018-10-20 21:13:24 +0300

    Prepared solution

---
+++ ServiceDestination/Controllers/SourceController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceDestination.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SourceController : ControllerBase
+    {
+
+    }
+}
---
+++ ServiceSource/Controllers/DestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceSource.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DestController : ControllerBase
+    {
+
+    }
+}
