c35c4aea23b864cf61fb11209a9565af160cd7d6
lijohnttle <lijohnttle@outlook.com>
2018-10-20 22:03:09 +0300

    Added retry logic

---
+++ ServiceDestination/Controllers/DestController.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceDestination.Controllers
+{
+    [ApiController]
+    public class DestController : ControllerBase
+    {
+        private static int _requestCouter;
+
+        [HttpGet("Retry")]
+        public async Task<IActionResult> Retry()
+        {
+            await Task.Delay(100);
+
+            if (Interlocked.Increment(ref _requestCouter) % 4 == 0)
+            {
+                return Ok(15);
+            }
+
+            return StatusCode((int) HttpStatusCode.InternalServerError, "Something went wrong");
+        }
+    }
+}
---
+++ ServiceSource/Controllers/SourceController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Polly;
+using Polly.Retry;
+
+namespace ServiceSource.Controllers
+{
+    [ApiController]
+    public class SourceController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly RetryPolicy<HttpResponseMessage> _httpRetryPolicy;
+
+
+        public SourceController()
+        {
+            _httpClient = new HttpClient
+            {
+                BaseAddress = new Uri("http://localhost:5001")
+            };
+
+            _httpRetryPolicy = Policy
+                .HandleResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+                .RetryAsync(3);
+        }
+
+
+        [HttpGet("Retry")]
+        public async Task<IActionResult> Retry()
+        {
+            const string requestEndpoint = "retry";
+
+            var response = await _httpRetryPolicy.ExecuteAsync(() =>
+                _httpClient.GetAsync(requestEndpoint));
+
+            if (response.IsSuccessStatusCode)
+            {
+                return Ok("Success");
+            }
+
+            return StatusCode((int) response.StatusCode, response.Content.ReadAsStringAsync());
+        }
+    }
+}
